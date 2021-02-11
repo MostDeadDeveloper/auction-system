1a571021a977f6e81fe1b81b75c71db974b103e7
RandomUser <>
2021-02-11 23:34:05 +0800

    Removed Unused third party apps

--- auction_system/auction_system/settings.py
+++ auction_system/auction_system/settings.py
@@ -44,12 +44,12 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
 
-    'rest_framework',
-    'drf_multiple_model',
+    # 'rest_framework',
+    # 'drf_multiple_model',
 
     'django_js_reverse',
 
-    'webpack_loader',
+    # 'webpack_loader',
 ]
 
 LOCAL_APPS = [
