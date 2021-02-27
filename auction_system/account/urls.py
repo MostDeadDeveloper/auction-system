88da66a6dd73307473f127aebc93c5acbe1d9111
RandomUser <>
2021-02-28 02:07:10 +0800

    Added Login Module

--- auction_system/account/urls.py
+++ auction_system/account/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
-#  from .views import 
+from .views import DashboardView, CustomLoginView
 
 app_name = 'account'
 
 urlpatterns = [
-
+    path(
+        'dashboard/',
+        DashboardView.as_view(),
+        name='dashboard',
+    ),
+    path(
+        'user/login/',
+        auth_views.LoginView.as_view(),
+        {'template_name': 'account/login.html'},
+        name='account_login',
+    ),
 ]
